3326850b328f2cfb23512e5678d528a327f5687c
mk980 <malickfiaz@aol.com>
2023-08-06 13:41:18 +0300

    .gitignore

---
+++ hello.py
@@ -0,0 +1,38 @@
+# # write a function to say hello 
+
+# def main ():
+#     name = input ("What's your name? ")
+#     print (hello (name))
+    
+# def hello (to = "world"):
+#     return f"hello, {to}"
+
+# if __name__ == "__main__":
+#     main()
+
+
+# ask user his name
+# name = input ("what's your name? ").strip () .title () 
+
+# #split the name into first and last name
+
+# first, last = name.split (" ")
+
+# #print the user name
+# print (f"Hello, {first}")
+
+
+def main ():
+
+    name = input("what's your name? ")
+    hello (name)
+
+def hello (to):
+    print ("hello,", to)
+    
+
+
+main ()
+
+
+
